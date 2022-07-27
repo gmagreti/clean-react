b3b7fc94aa61c102d3a816317379767667d6eba6
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-07-27 01:21:45 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,8 +1,10 @@
 import faker from 'faker'
 
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { mockAuthentication } from '@/domain/test/mock-authentication'
+import { InvalidCredentialsError } from '@/domain/erros/invalid-credentials-error'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -12,7 +14,6 @@ type SutTypes = {
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
-
   return {
     sut, httpPostClientSpy
   }
@@ -22,21 +23,23 @@ describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
     const url = faker.internet.url()
     const {sut, httpPostClientSpy} = makeSut(url)
-
     await sut.auth(mockAuthentication())
-
     expect(httpPostClientSpy.url).toBe(url)
   })
-})
 
-describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct body', async () => {
     const {sut, httpPostClientSpy} = makeSut()
-
     const authenticationParams = mockAuthentication()
-
     await sut.auth(authenticationParams)
-
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+
+  test('Should throw InvalidCredentialsError if HttpPostCloent returns 401', async () => {
+    const {sut, httpPostClientSpy} = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unathorized
+    }
+    const promisse = sut.auth(mockAuthentication())
+    await expect(promisse).rejects.toThrow(new InvalidCredentialsError())
   })
 })
