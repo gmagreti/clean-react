cf157fda8855219e174296d544a73ffe32e1f571
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-01-11 12:08:24 -0300

    test: ensure AxiosHttpClient calls with corret body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -18,12 +18,13 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL and verb', async () => {
+  test('Should call axios with correct values', async () => {
     const request = mockPostRequest()
     const sut = makeSut()
     await sut.post({
       url: request.url
     })
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+
   })
 })
